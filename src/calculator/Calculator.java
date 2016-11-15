3ea2b543c73d7b5b740c7e35f188d35239b75e0b
SKapturska <skapturska@us.edu.pl>
2016-11-15 10:07:36 +0100

    Start

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Window win = new Window();
+        win.setVisible(true);
+    }
+    
+}
---
+++ src/calculator/Window.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+
+public class Window extends JFrame implements ActionListener {
+
+    
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+    }
+
+}
